7983a25938c530dedd1fba0b2a28705fc30a4d99
bfisher777 <bfisher777@gmail.com>
2016-11-10 21:24:03 -0800

    again

--- how-to/public/base.js
+++ how-to/public/base.js
@@ -1,6 +1,5 @@
-//http://stackoverflow.com/questions/19397140/collapsing-sidebar-with-bootstrap-3
-$(document).ready(function() {
-  $('[data-toggle=offcanvas]').click(function() {
-    $('.row-offcanvas').toggleClass('active');
-  });
-});
+//
+$("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
