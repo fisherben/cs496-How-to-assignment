8a0d4c56abe3a1c93e25ab16be0ca9265248a53d
bfisher777 <bfisher777@gmail.com>
2016-11-10 21:51:00 -0800

    nav bar

--- how-to/public/base.js
+++ how-to/public/base.js
@@ -1,5 +1,7 @@
 //
-$("#menu-toggle").click(function(e) {
+$( document ).ready(function() {
+	$("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
-    });
+	});
+});
